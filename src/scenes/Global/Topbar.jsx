906d7eca65cc7b5b77a6b74d55f9f867dd00921e
Christopher <Chris1Bobsy@Gmail.com>
2023-01-26 12:54:51 +0100

    rerite icon imports

--- src/scenes/Global/Topbar.jsx
+++ src/scenes/Global/Topbar.jsx
@@ -2,14 +2,12 @@ import { useContext } from "react";
 import { Box, IconButton, InputBase, useTheme } from "@mui/material";
 
 import { ColorModeContext, tokens } from "../../theme";
-import {
-  DarkModeOutlined,
-  LightModeOutlined,
-  NotificationsOutlined,
-  PersonOutline,
-  Search,
-  SettingsOutlined,
-} from "@mui/icons-material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Topbar = () => {
   const theme = useTheme();
@@ -26,7 +24,7 @@ const Topbar = () => {
       >
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
         <IconButton type="button" sx={{ p: 1 }}>
-          <Search />
+          <SearchIcon />
         </IconButton>
       </Box>
 
@@ -34,19 +32,19 @@ const Topbar = () => {
       <Box display="flex">
         <IconButton type="button">
           {theme.palette.mode === "dark" ? (
-            <DarkModeOutlined />
+            <DarkModeOutlinedIcon />
           ) : (
-            <LightModeOutlined />
+            <LightModeOutlinedIcon />
           )}
         </IconButton>
         <IconButton type="button">
-          <NotificationsOutlined />
+          <NotificationsOutlinedIcon />
         </IconButton>
         <IconButton type="button">
-          <SettingsOutlined />
+          <SettingsOutlinedIcon />
         </IconButton>
         <IconButton type="button">
-          <PersonOutline />
+          <PersonOutlinedIcon />
         </IconButton>
       </Box>
     </Box>
