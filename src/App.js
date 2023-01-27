a89757cc05b181e44e950508f32f4481963002d5
Christopher <Chris1Bobsy@Gmail.com>
2023-01-27 17:21:59 +0100

    create sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/Global/Topbar";
+import Sidebar from "./scenes/Global/Sidebar";
 import { ColorModeContext, useMode } from "./theme";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
           </main>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ProSidebarProvider } from "react-pro-sidebar";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ProSidebarProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ProSidebarProvider>
   </React.StrictMode>
 );
