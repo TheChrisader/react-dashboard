aab3b078c3a617ebe7393a2516fdfc760c712e47
Christopher <Chris1Bobsy@Gmail.com>
2023-01-29 18:41:06 +0100

    create Dashboard and Team pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/Global/Topbar";
 import Sidebar from "./scenes/Global/Sidebar";
+import Dashboard from "./scenes/Dashboard";
 import { ColorModeContext, useMode } from "./theme";
+import Team from "./scenes/Team";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -14,6 +17,10 @@ function App() {
           <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
