6623fa7d012f3b58cfb868d399a503ced77a2087
Christopher <Chris1Bobsy@Gmail.com>
2023-01-30 15:32:00 +0100

    create Form page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Contacts from "./scenes/Contacts";
 import Invoices from "./scenes/Invoices";
 import { ColorModeContext, useMode } from "./theme";
 import Team from "./scenes/Team";
+import Form from "./scenes/Form";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -24,6 +25,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
             </Routes>
           </main>
         </div>
